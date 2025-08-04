e7c187927b6c0235b47d14a4cdc30ffcf93c696b
chinotrunk <gerson2050@hotmail.com>
2025-08-04 13:43:58 -0500

    feat: agregar función de sorteo aleatorio y ajustar mensaje de alerta

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function agregarAmigo() {
   let nombre = input.value.trim();
 
   if ( nombre === "") {
-    alert('Por favor, ingresa un nombre válido')
+    alert('Por favor, inserte un nombre.')
     return;
   }
 
@@ -26,5 +26,27 @@ function actualizarListaAmigos() {
   }
 }
 
+function sortearAmigo() {
+  let resultado = document.getElementById("resultado");
+  let lista = document.getElementById("listaAmigos");
 
+  resultado.innerHTML = ""; 
+
+  if (amigos.length === 0) {
+    alert("No hay amigos para sortear.");
+    return;
+  }
+
+  let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+  let amigoSorteado = amigos[indiceAleatorio];
+
+  let li = document.createElement("li");
+  li.textContent = `El amigo secreto es: ${amigoSorteado}`;
+  resultado.appendChild(li);
+
+  lista.innerHTML = "";
+
+  amigos.splice(indiceAleatorio, 1);
+
+}
 
